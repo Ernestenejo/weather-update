8cd33a71ae22f876757734ae56c08bef580e7f25
Ernestenejo <eenejo69@gmail.com>
2025-03-22 12:35:47 +0100

    server update

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 require('./config/database')
+const cors = require('cors')
 const weatherRouter = require('./route/weatherRouter')
 const PORT = process.env.PORT
 
@@ -8,6 +9,7 @@ const app = express();
 
 app.use(express.json())
 app.use('/api/v1', weatherRouter)
+app.use(cors())
 
 app.listen(PORT, ()=>{
 console.log(`server is listening to PORT: ${PORT}`)
